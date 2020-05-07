ed85761f3de5ea3374b7735d06d99a9b6cb07b87
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-05-07 16:29:12 +0000

    Automatically backed up by Learn

--- db/migrate/20200507162825_user.rb
+++ db/migrate/20200507162825_user.rb
@@ -1,4 +1,9 @@
-class User < ActiveRecord::Migration[5.1]
+class Users < ActiveRecord::Migration[5.1]
   def change
+    create_table :users do |t|
+   t.string :username
+   t.string :password
+   t.integer :balance
+ end
   end
 end
