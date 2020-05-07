7c5f8a72a939a2556ba878965ac12546405b3936
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-05-07 16:42:01 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,4 +18,10 @@ ActiveRecord::Schema.define(version: 20200507163439) do
     t.integer "balance"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.integer "balance"
+  end
+
 end
