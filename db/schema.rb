39d459a12e21781a397a025944871b4e9718f627
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-05-07 17:01:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200507163439) do
+ActiveRecord::Schema.define(version: 20200507170109) do
 
   create_table "user", force: :cascade do |t|
     t.string "username"
     t.string "password"
     t.integer "balance"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
   end
 
 end
